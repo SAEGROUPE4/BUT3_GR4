2be09da70e0237068f31c362f4b00d7c527968e0
Xzoudy <68783419+Xzoudy@users.noreply.github.com>
2025-01-20 19:24:05 +0100

    ajout test banquemanagaer

--- _00_ASBank2023/src/test/java/com/iut/banque/test/facade/TestsBanqueManager.java
+++ _00_ASBank2023/src/test/java/com/iut/banque/test/facade/TestsBanqueManager.java
@@ -1,7 +1,15 @@
 package com.iut.banque.test.facade;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
+import com.iut.banque.exceptions.IllegalFormatException;
+import com.iut.banque.exceptions.InsufficientFundsException;
+import com.iut.banque.exceptions.TechnicalException;
+import com.iut.banque.modele.Client;
+import com.iut.banque.modele.Compte;
+import com.iut.banque.modele.CompteAvecDecouvert;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,6 +20,8 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.iut.banque.exceptions.IllegalOperationException;
 import com.iut.banque.facade.BanqueManager;
+
+import java.util.Map;
 
 //@RunWith indique à JUnit de prendre le class runner de Spirng
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -167,5 +177,111 @@ public class TestsBanqueManager {
 			fail("Une Exception " + te.getClass().getSimpleName() + " a été récupérée");
 		}
 	}
+
+	@Test
+	public void testCrediter() {
+		Compte compte = bm.getAccountById("SA1011011011");
+		if (compte == null) {
+			fail("Le compte n'existe pas.");
+		}
+
+		double soldeInitial = compte.getSolde();
+		double montant = 100.0;
+
+		try {
+			bm.crediter(compte, montant);
+
+			assertEquals("Le compte n'a pas été crédité correctement.", soldeInitial + montant, compte.getSolde(), 0.001);
+		} catch (IllegalFormatException e) {
+			fail("Aucune exception ne devrait être levée pour un montant valide.");
+		}
+	}
+
+	@Test
+	public void testDebiter() {
+		Compte compte = bm.getAccountById("SA1011011011");
+		if (compte == null) {
+			fail("Le compte n'existe pas.");
+		}
+
+		double soldeInitial = compte.getSolde();
+		double montant = 2.0;
+
+		try {
+			bm.debiter(compte, montant);
+
+			assertEquals("Le compte n'a pas été debite correctement.", soldeInitial - montant, compte.getSolde(), 0.001);
+		} catch (IllegalFormatException e) {
+			fail("Aucune exception ne devrait être levée pour un montant valide.");
+		} catch (InsufficientFundsException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+	@Test
+	public void testCreateManager()  {
+		try {
+			bm.createManager("1234567891", "tusaisdeja", "managernom", "prenom", "adresse", true);
+		} catch (TechnicalException e) {
+			throw new RuntimeException(e);
+		} catch (IllegalFormatException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Test
+	public void testGetAllManager()  {
+        Map<String, Client> managers = bm.getAllManagers();
+
+		//if (managers == null || managers.isEmpty()) {
+		//	fail("erreur lors de la recuperation des manageurs");
+		//}
+	}
+
+	@Test
+	public void testChangeDecouvert() {
+
+		CompteAvecDecouvert compte = (CompteAvecDecouvert) bm.getAccountById("AV1011011011");
+		if (compte == null) {
+			fail("Le compte avec découvert n'existe pas dans la base de données.");
+		}
+
+		double decouvertInitial = compte.getDecouvertAutorise();
+		double nouveauDecouvert = decouvertInitial + 100.0;
+
+		try {
+			bm.changeDecouvert(compte, nouveauDecouvert);
+
+			CompteAvecDecouvert updatedCompte = (CompteAvecDecouvert) bm.getAccountById("AV1011011011");
+			if (updatedCompte == null) {
+				fail("Le compte n'a pas été récupéré après mise à jour.");
+			}
+
+			assertEquals("Le découvert autorisé n'a pas été correctement mis à jour.", nouveauDecouvert, updatedCompte.getDecouvertAutorise(), 0.001);
+		} catch (Exception e) {
+			fail("Aucune exception ne devrait être levée : " + e.getMessage());
+		}
+	}
+
+	@Test
+	public void testCreateAccount() {
+
+		Client client = (Client) bm.getUserById("c.exist");
+		if (client == null) {
+			fail("Le client n'existe pas dans la base de données.");
+		}
+		String numeroCompte = "NW2022010001";
+
+		try {
+			bm.createAccount(numeroCompte,client);
+		} catch (TechnicalException | IllegalFormatException e) {
+			fail("Aucune exception ne devrait être levée lors de la création du compte : " + e.getMessage());
+		}
+	}
+
+
+
+
 
 }
